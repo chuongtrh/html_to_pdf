e420408765dbfb600205d956ec764f004fb7d74d
ChuongTran <chuongtrh@gmail.com>
2019-08-03 13:41:24 +0700

    ~ Add try/catch

--- pdf.js
+++ pdf.js
@@ -3,10 +3,10 @@ const path = require("path");
 const puppeteer = require('puppeteer');
 const handlebars = require("handlebars");
 
-(async () => {
-
-    var dataBinding = {
-        items: [{
+try {
+    (async () => {
+        var dataBinding = {
+            items: [{
                 name: "item 1",
                 price: 100
             },
@@ -18,35 +18,40 @@ const handlebars = require("handlebars");
                 name: "item 3",
                 price: 300
             }
-        ],
-        total: 600,
-        isWatermark: true
-    }
-    
-    var templateHtml = fs.readFileSync(path.join(process.cwd(), 'invoice.html'), 'utf8');
-    var template = handlebars.compile(templateHtml);
-    var finalHtml = template(dataBinding);
-    var options = {
-        format: 'A4',
-        headerTemplate: "<p></p>",
-        footerTemplate: "<p></p>",
-        displayHeaderFooter: false,
-        margin: {
-            top: "40px",
-            bottom: "100px"
-        },
-        printBackground: true,
-        path: 'invoice.pdf'
-    }
+            ],
+            total: 600,
+            isWatermark: true
+        }
 
-    const browser = await puppeteer.launch({
-        args: ['--no-sandbox'],
-        headless: true
-    });
-    const page = await browser.newPage();
-    await page.goto(`data:text/html,${finalHtml}`, {
-        waitUntil: 'networkidle0'
-    });
-    await page.pdf(options);
-    await browser.close();
-})();
+        var templateHtml = fs.readFileSync(path.join(process.cwd(), 'invoice.html'), 'utf8');
+        var template = handlebars.compile(templateHtml);
+        var finalHtml = template(dataBinding);
+        var options = {
+            format: 'A4',
+            headerTemplate: "<p></p>",
+            footerTemplate: "<p></p>",
+            displayHeaderFooter: false,
+            margin: {
+                top: "40px",
+                bottom: "100px"
+            },
+            printBackground: true,
+            path: 'invoice.pdf'
+        }
+
+        const browser = await puppeteer.launch({
+            args: ['--no-sandbox'],
+            headless: true
+        });
+        const page = await browser.newPage();
+        await page.goto(`data:text/html,${finalHtml}`, {
+            waitUntil: 'networkidle0'
+        });
+        await page.pdf(options);
+        await browser.close();
+
+        console.log('Done: invoice.pdf is created!')
+    })();
+} catch (err) {
+    console.log('ERROR:', err);
+}
